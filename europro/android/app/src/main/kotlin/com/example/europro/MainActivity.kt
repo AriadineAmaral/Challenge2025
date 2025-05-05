4d3023426b09ed1eb9506e9f344b8540b5de14ba
Yasmin Miguez <yasminmiguez@outlook.com>
2025-05-04 23:31:23 -0300

    adicionando projeto EuroPro: splash screen e tela inicial/login seguindo prototipo do Figma e divisão de arquivos por componente. versão inicial

---
+++ europro/android/app/src/main/kotlin/com/example/europro/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.europro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ europro/lib/screens/login_screen.dart
@@ -0,0 +1,49 @@
+// lib/screens/login_screen.dart
+import 'package:flutter/material.dart';
+import '../widgets/button.dart';
+
+class EuroProLoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 32.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'images/logoEuroPro.png',
+                width: 250,
+              ),
+              SizedBox(height: 80),
+              Button(
+                text: 'Acessar conta',
+                backgroundColor:const Color(0xFF00358E),
+                textColor: Colors.white,
+                onPressed: () {},
+              ),
+              SizedBox(height: 16),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Primeiro acesso?',
+                  style: TextStyle(fontSize: 14, color: Colors.black87),
+                ),
+              ),
+              SizedBox(height: 8),
+              Button(
+                text: 'Criar conta',
+                backgroundColor:const Color(0xFFFFF200),
+                textColor: Colors.black,
+                isBold: true,
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
