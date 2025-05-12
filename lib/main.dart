9e1248e4356115df7a7804d41aa485c5ffd8e6f6
Samuel <samuelabreu2346@gmail.com>
2025-05-11 21:42:34 -0300

    feat - criando tela inicial e de login com animacao

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+// lib/main.dart
+import 'package:europro/screens/initial_screen.dart';
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(EuroProApp());
+}
+
+class EuroProApp extends StatelessWidget {
+  const EuroProApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: InitialScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
---
+++ lib/widgets/button.dart
@@ -0,0 +1,44 @@
+
+import 'package:flutter/material.dart';
+
+class Button extends StatelessWidget {
+  final String text;
+  final Color backgroundColor;
+  final Color textColor;
+  final VoidCallback onPressed;
+  final bool isBold;
+
+  const Button({
+    super.key,
+    required this.text,
+    required this.backgroundColor,
+    required this.textColor,
+    required this.onPressed,
+    this.isBold = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: backgroundColor,
+          padding: EdgeInsets.symmetric(vertical: 16),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.zero,
+          ),
+        ),
+        child: Text(
+          text,
+          style: TextStyle(
+            fontSize: 16,
+            color: textColor,
+            fontWeight: isBold ? FontWeight.bold : FontWeight.normal,
+          ),
+        ),
+      ),
+    );
+  }
+}
